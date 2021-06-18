36df3dfef280923a49610fc3608e1aa81abf53e2
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-06-18 22:55:55 +0100

    first commit

---
+++ src/stories/SampleLoader.stories.js
@@ -0,0 +1,73 @@
+import SampleLoaderComponent from "./SampleLoaderComponent.svelte";
+import fetch from "node-fetch";
+
+export default {
+  component: SampleLoaderComponent,
+  title: "LoaderExample",
+};
+
+// Initial approach renders undefined
+/* 
+export const SampleStory = (args, { loaded: { todo } }) => ({
+  Component: SampleLoaderComponent,
+  props: {
+    ...args,
+    todo,
+  },
+});
+
+SampleStory.loaders = [
+  async () => ({
+    todo: (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json(),
+  }),
+];
+ */
+
+export const SampleStory = (args, { loaded: { todo } }) => {
+  console.log(`todo:${JSON.stringify(todo, null, 2)}`);
+  return {
+    Component: SampleLoaderComponent,
+    props: {
+      ...args,
+      todo,
+    },
+  };
+};
+// using async here returns =>todo:[object Promise]
+/* SampleStory.loaders = [
+  async () => ({
+    todo: (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json(),
+  }),
+]; */
+
+SampleStory.loaders = [
+  async () => ({
+    todo: await fetchTodo(),
+  }),
+];
+
+async function fetchTodo() {
+  try {
+    const fetchRequest = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const response = fetchRequest.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// global (preview.js)
+export const AnotherStory = (args, { loaded: { currentUser } }) => {
+  console.log(`currentUser:${JSON.stringify(currentUser, null, 2)}`);
+  return {
+    Component: SampleLoaderComponent,
+    props: {
+      ...args,
+      todo: currentUser,
+    },
+  };
+};
+
+//
---
+++ src/stories/SampleLoaderComponent.svelte
@@ -0,0 +1,19 @@
+<script>
+  // todo props
+  export let todo = {
+    userId: 0,
+    id: 0,
+    title: "Sanple TODO",
+    completed: false,
+  };
+
+</script>
+
+<div>
+  <h1>This is a sample component to demonstrate loaders</h1>
+  <h2>Todo props has the following inside</h2>
+  <h3>userId:{todo.userId}</h3>
+  <h3>id:{todo.id}</h3>
+  <h3>title:{todo.title}</h3>
+  <h3>completed:{todo.completed}</h3>
+</div>
